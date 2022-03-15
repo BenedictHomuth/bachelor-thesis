3672a6db53dc5b9248624c05426998bf9dc0a79f
Benedict Homuth <bhomuth@inovex.de>
2022-03-15 09:06:19 +0100

    Added security group for public web access

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,7 @@ provider "aws" {
 resource "aws_instance" "test_ec2_amd64" {
   ami                    = "ami-00e76d391403fc721" # Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.terraform_public.id]
+  vpc_security_group_ids = [aws_security_group.terraform_public.id, aws_security_group.webserver_public.id]
   key_name               = "terraform_test"
   user_data              = var.docker_setup
 
@@ -41,14 +41,13 @@ resource "aws_instance" "test_ec2_arm64" {
 }
 
 
-# Creating a security group
+# Security Group Terraform
 resource "aws_security_group" "terraform_public" {
   name        = "terraform_public_ssh"
   description = "Allows for ssh into the ec2 test instance"
   vpc_id      = var.vpc_id
 }
 
-# Rules for the Security Group
 resource "aws_security_group_rule" "public_out" {
   type        = "egress"
   from_port   = 0
@@ -68,3 +67,32 @@ resource "aws_security_group_rule" "public_in_ssh" {
 
   security_group_id = aws_security_group.terraform_public.id
 }
+
+
+# Security Group WEB
+resource "aws_security_group" "webserver_public" {
+  name        = "webserver_public"
+  description = "Allows for http connections on port 80"
+  vpc_id      = var.vpc_id
+}
+
+resource "aws_security_group_rule" "public_web_in" {
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.webserver_public.id
+}
+
+resource "aws_security_group_rule" "public_web_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0 
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.webserver_public.id
+}
+
