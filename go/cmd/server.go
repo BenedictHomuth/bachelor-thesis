6f60e2ef1aec6ec1c5fe90a0740637b51ed2cbb4
Benedict Homuth <bhomuth@inovex.de>
2022-03-31 14:41:24 +0200

    Test db connection in k3s cluster

--- go/cmd/server.go
+++ go/cmd/server.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
 
 func handleBasicRequest(w http.ResponseWriter, r *http.Request) {
@@ -11,6 +14,22 @@ func handleBasicRequest(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
+	db, err := sql.Open("postgres", "host=postgres user=dbUser password=helloWorld! dbName=todo-db")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+
+	//
+
 	http.HandleFunc("/", handleBasicRequest)
 
 	fmt.Printf("Starting server at port 8080\n")
