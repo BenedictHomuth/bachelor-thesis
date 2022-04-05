f535008e00680c80b1a928db8ffe1fda261f7860
Benedict Homuth <bhomuth@inovex.de>
2022-04-05 19:03:30 +0200

    k6 test written, containered, ci

--- k6/apiTest.js
+++ k6/apiTest.js
@@ -1,7 +1,31 @@
 import http from 'k6/http'
 import { sleep } from 'k6'
 
+export const options = {
+    stages: [
+        {duration: "10s", target: 10},
+        {duration: "10s", target: 15},
+        {duration: "10s", target: 0},
+    ]
+}
+
 export default function(){
-    http.get('http://host.docker.internal:8080')
-    sleep(1)
+    const linkGetAll = `${__ENV.HOSTNAME}/get`
+    const linkCreate = `${__ENV.HOSTNAME}/create`
+    const payload = JSON.stringify({
+        title: 'todo',
+        description: 'go to work!',
+      })
+    const params = {
+         headers: {
+          'Content-Type': 'application/json',
+         },
+     }
+    
+    //  Execution 
+      http.post(linkCreate, payload, params)
+      sleep(1)
+
+      http.get(linkGetAll)
+      sleep(1)
 }
